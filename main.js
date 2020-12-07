96143324b03f6523d3eb835aba0b8a2e20948ff3
zhanggx01 <zhanggx01@jsfund.cn>
2020-12-07 21:18:20 +0800

    feat:日常代码提交

--- main.js
+++ main.js
@@ -4,13 +4,13 @@
  * @Author: zhanggx01
  * @Date: 2020-12-03 18:47:54
  * @LastEditors: zhanggx01
- * @LastEditTime: 2020-12-03 20:42:27
+ * @LastEditTime: 2020-12-07 20:52:09
  */
 
 import App from './src/App.js'
 
 import {createApp} from './src/runtime-Canvas/index'
-import {game} from './src/Game'
+import {getRootContainer} from './src/Game'
 
 
 
@@ -18,4 +18,4 @@ import {game} from './src/Game'
 // 1. 创建根组件
 // 2. 创建根容器
 // createApp(rootComponent).mount(rootContainer)
-createApp(App).mount(game.stage)
+createApp(App).mount(getRootContainer())
--- src/Game.js
+++ src/Game.js
@@ -4,7 +4,7 @@
  * @Author: zhanggx01
  * @Date: 2020-12-03 20:40:04
  * @LastEditors: zhanggx01
- * @LastEditTime: 2020-12-03 20:43:04
+ * @LastEditTime: 2020-12-07 20:52:38
  */
 import {Application} from 'pixi.js'
 // 初始化game
@@ -13,3 +13,7 @@ export const game = new Application({
   height: 1080
 })
 document.body.append(game.view)
+export function getRootContainer () {
+  // debugger
+  return game.stage
+}
